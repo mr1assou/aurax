26f05ea5555c19bb21c45e69dad2c73b1832d801
Marwane Assou <marwane.assou@gmail.com>
2025-02-28 00:19:00 +0100

    https traffic

--- frontend/src/Axios.jsx
+++ frontend/src/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://aurax.ma:3001',  // Your backend API URL
+    baseURL: 'https://dhfghdfdhjgdfhjg.aurax.ma',  // Your backend API URL
     withCredentials: true,  // Ensures cookies are sent automatically
 });
 
