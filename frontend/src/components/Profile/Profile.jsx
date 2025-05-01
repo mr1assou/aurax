d19e65a0c425f79f3a46af9d15b9b83aa4bd0933
Marwane Assou <marwane.assou@gmail.com>
2025-05-01 02:53:14 +0200

    rating

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -8,12 +8,7 @@ function Profile() {
         <div className='mt-5 px-1'>
             
             <UserDetails/>
-            <div className='mt-[140px] max:mt-[200px] text-white px-8'>
-                <div action="" className='flex border-2 border-white rounded-lg py-2 px-5'>
-                    <input type="text" className='w-full bg-black py-5 outline-none' placeholder='add post'/>
-                    <button>+</button>
-                </div>
-            </div>
+            <div className='mt-20'></div>
             <Posts/>
         </div>
     )
