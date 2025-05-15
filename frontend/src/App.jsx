3eeb7a9310da43c3fbf09d0d3c0cd3feb87a24fc
Marwane Assou <marwane.assou@gmail.com>
2025-05-15 22:16:27 +0200

    aurax

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Rooms from "./components/Rooms/Rooms";
 import VideoRoom from "./components/Rooms/VideoRoom";
 import Profile from "./components/Profile/Profile";
 import Events from "./components/Events";
-import Marketplace from "./components/Marketplace";
+import Marketplace from "./components/MarketPlace";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
